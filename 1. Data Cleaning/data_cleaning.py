d0fb99605f38dff561029af992f10c3da5c4c584
namanlp <naman@garudalinux.org>
2023-09-21 23:22:36 +0530

    Data Cleaning Program

---
+++ 1. Data Cleaning/data_cleaning.py
@@ -0,0 +1,24 @@
+# Made by Naman Garg
+# Contact : <12112061@nitkkr.ac.in>
+
+import pandas as pd
+
+# The task did not require much cleaning,
+# because already cleaned data was provided
+
+# importing dataset
+dataset = pd.read_csv("../music_data_source.csv")
+# pd.set_option('display.max_columns', None)
+
+# Attributes
+attributes = list(dataset.columns)
+# print(attributes)
+
+print(dataset.describe())
+# Now, we can clearly see that length is same for complete dataset
+# So, we can remove length
+dataset.drop(['length'], axis=1, inplace=True)
+print(dataset.describe())
+
+# Save it to CSV
+dataset.to_csv("music_data_source_cleaned.csv", index=False)
